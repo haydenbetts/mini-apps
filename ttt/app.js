42c9c6b1a7c11938c7f23d7d7dd420a37387ac3c
haydenbetts <haydenbetts@gmail.com>
2018-12-07 17:11:10 -0800

    updating board to current player based on clicks

--- ttt/app.js
+++ ttt/app.js
@@ -5,19 +5,35 @@ class TTT {
       [" ", "X", " "],
       [" ", " ", "X"]
     ]
+
+    this.currentPlayer = "X";
+    this.handleClick = this.handleClick.bind(this);
+    this.updateBoard = this.updateBoard.bind(this);
+  }
+
+  updateBoard(player, row, cell) {
+    this.board[row][cell] = player;
+    this.drawBoard(this.board);
+  }
+
+  handleClick(node) {
+    let rowNum = node.target.id.split('-')[1];
+    let cellNum = node.target.id.split('-')[3];
+    console.log(rowNum, cellNum);
+    this.updateBoard(this.currentPlayer, rowNum, cellNum);
   }
 
   drawBoard(board) {
     board.forEach((row, i) => {
-      let rowNode = document.getElementById(`row-${i}`);
-
-      row.forEach((cell) => {
-        let buttonNode = document.createElement("button");
+      row.forEach((cell, j) => {
+        let buttonNode = document.getElementById(`row-${i}-cell-${j}`);
         buttonNode.innerText = cell;
-        rowNode.append(buttonNode);
+        buttonNode.addEventListener("click", this.handleClick)
       });
     });
   }
+
+
 }
 
 let Game = new TTT;
