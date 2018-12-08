b5d2aae787c8af747a1270d93bd7d7d5a13e8dc3
haydenbetts <haydenbetts@gmail.com>
2018-12-07 16:34:12 -0800

    refactored into functional style

--- ttt/app.js
+++ ttt/app.js
@@ -1,15 +1,24 @@
-let board = [
-  ["X", " ", " "],
-  [" ", "X", " "],
-  [" ", " ", "X"]
-]
+class TTT {
+  constructor() {
+    this.board = [
+      ["X", " ", " "],
+      [" ", "X", " "],
+      [" ", " ", "X"]
+    ]
+  }
 
-board.forEach((row, i) => {
-  let rowNode = document.getElementById(`row-${i}`);
+  drawBoard(board) {
+    board.forEach((row, i) => {
+      let rowNode = document.getElementById(`row-${i}`);
 
-  row.forEach((cell) => {
-    let buttonNode = document.createElement("button");
-    buttonNode.innerText = cell;
-    rowNode.append(buttonNode);
-  });
-});
+      row.forEach((cell) => {
+        let buttonNode = document.createElement("button");
+        buttonNode.innerText = cell;
+        rowNode.append(buttonNode);
+      });
+    });
+  }
+}
+
+let Game = new TTT;
+Game.drawBoard(Game.board);
